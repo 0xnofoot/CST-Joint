d83e0a06528a1f05b4f16bbb048d254d1c8dd0f3
nofoot <x0xnofoot@163.com>
2023-06-19 11:13:57 +0800

    2023.06.19

--- core/global_var.py
+++ core/global_var.py
@@ -6,7 +6,7 @@ rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
 # CST的python库路径，自己按照CST安装路径修改
-CST_HOME = r"D:\CST Micro Studio\AMD64\python_cst_libraries"
+CST_HOME = r"D:\CST\AMD64\python_cst_libraries"
 
 WORK_HOME = rootPath
 
